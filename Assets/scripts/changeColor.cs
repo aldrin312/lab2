dd863a203976b9a90ea3255f083abef1fc65a57a
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-09-15 23:22:12 -0400

    lab2

---
+++ Assets/scripts/changeColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class changeColor : MonoBehaviour {
+
+	public GameObject img1;
+	public GameObject img2;
+	public GameObject imgclick;
+
+	void OnMouseDown()
+	{
+		imgclick.SetActive(true);
+		img1.SetActive(false);
+		img2.SetActive(false);
+	}
+}
